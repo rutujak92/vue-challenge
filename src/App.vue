ebaf87675a8ddbcfae96fa56f07487b3cf31e574
rutujak92 <31957160+rutujak92@users.noreply.github.com>
2019-03-27 18:44:15 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Graph/>
+  </div>
+</template>
+
+<script>
+import Graph from "./components/AppGraph.vue";
+
+export default {
+  name: "app",
+  components: {
+    Graph
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding:0 30px;
+}
+</style>
